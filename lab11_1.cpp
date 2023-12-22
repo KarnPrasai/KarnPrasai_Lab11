ecd90470ab8722758d178c6d2700d338b3ad6695
KarnPrasai <karn_prasai@cmu.ac.th>
2023-12-22 14:53:24 +0700

    lab11_1 completed

--- lab11_1.cpp
+++ lab11_1.cpp
@@ -1,2 +1,26 @@
-// cout << "Press Enter 3 times to reveal your future.";
-// cout << "You will get A in this 261102.";
+#include <iostream>
+#include <cstdlib>
+#include <string>
+#include <ctime>
+
+using namespace std;
+
+string grade[]={"A","B+","B","C+","C","D+","D","F","W"};
+int yourgrade;
+
+void getgrade()
+{
+    yourgrade = rand() % 9;
+}
+
+int main()
+{
+    cout << "Press Enter 3 times to reveal your future.";
+    for(int i = 1; i<=3;i++)
+    {
+        cin.get();
+    }
+    srand(time(0));
+    getgrade();
+    cout << "You will get " << grade[yourgrade] << " in this 261102.";
+}
